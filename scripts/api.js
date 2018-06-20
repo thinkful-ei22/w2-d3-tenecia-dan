9d39febc348b14b3ccd2cb9a59d3d68ee013512d
Daniyar Salikhov <salihovdr@gmail.com>
2018-06-20 14:40:26 -0400

    fixed getItem and createItem errors

--- scripts/api.js
+++ scripts/api.js
@@ -1,16 +1,17 @@
 'use strict';
+/*global $ */
 
 const api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/dan';
 
   const getItems = function(callback){
-    callback('api module works!');
+    $.getJSON(`${BASE_URL}/items`, callback);
   };
 
   const createItem = (name, callback) => {
     const newItem = JSON.stringify({name: 'Bananas'});
     $.ajax({
-      url: BASE_URL,
+      url: `${BASE_URL}/items`,
       method: 'POST',
       contentType: 'application/json',
       data: newItem,
